b9344ad3df407ce31ebe7d8c30781374c2b44b60
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2021-06-17 08:43:41 +0200

    Revert "Bump tests"
    This reverts commit 867069f4cf3172f6ae9214f9afee14b2c7b441fc.

--- src/__tests__/getNodeVersion.test.ts
+++ src/__tests__/getNodeVersion.test.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-import { findPackageJson, getNodeVersion, getNpmVersion } from '../getNodeVersion';
+import { findPackageJson, getNodeVersion } from '../getNodeVersion';
 
 const fixturePath = './src/__tests__/fixture';
 const fixture = `./src/__tests__/fixture/package.json`;
@@ -15,18 +15,10 @@ describe('getNodeVersion', () => {
   });
 
   describe('getNodeVersion', () => {
-    test('get node version text within package.json', () => {
+    test('get version text within package.json', () => {
       const result = getNodeVersion(fixturePath);
 
-      expect(result).toBe('>= 14.15.5');
-    });
-  });
-
-  describe('getNpmVersion', () => {
-    test('get npm version text within package.json', () => {
-      const result = getNpmVersion(fixturePath);
-
-      expect(result).toBe('^6.14.13');
+      expect(result).toBe('12.13.x');
     });
   });
 });
