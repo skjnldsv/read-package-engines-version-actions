814137910fbcb239cdf2825dbf49e90259c18ee9
John Molakvoæ <skjnldsv@users.noreply.github.com>
2021-06-17 08:32:57 +0200

    Return nodeVersion & npmVersion

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-import { getNodeVersion } from './getNodeVersion';
+import { getNodeVersion, getNpmVersion } from './getNodeVersion';
 
 async function run() {
   try {
@@ -7,10 +7,12 @@ async function run() {
 
     core.debug(`Load package.json at ${path}`);
 
-    const result = getNodeVersion(path);
+    const nodeVersion = getNodeVersion(path);
+    const npmVersion = getNpmVersion(path);
 
-    core.debug(`set output: version: ${result}`);
-    core.setOutput('version', result);
+    core.debug(`nodeVersion: ${nodeVersion}, npmVersion: ${npmVersion}`);
+    core.setOutput('nodeVersion', nodeVersion);
+    core.setOutput('npmVersion', npmVersion);
   } catch (error) {
     core.setFailed(error.message);
   }
